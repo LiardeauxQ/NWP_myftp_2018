daf9d2c1e57e3ecf9e101673ad84c458c21dd461
Quentin Liardeaux <quentinliardeaux@localhost.localdomain>
2019-04-14 21:24:26 +0200

    Add -l by default for LIST command

--- FTPServer/sources/commands/list.c
+++ FTPServer/sources/commands/list.c
@@ -7,6 +7,23 @@
 
 #include "server.h"
 
+static char **check_list_options(const char *param)
+{
+    char **split_cmd = str_to_word_array(param, " \t");
+    size_t size = count_2d_array(split_cmd);
+
+    if (size < 1)
+        return (NULL);
+    free(split_cmd[0]);
+    split_cmd[0] = strdup("ls");
+    split_cmd = realloc(split_cmd, (size + 2) * sizeof(char*));
+    for (int i = size ; i > 1 ; i--)
+        split_cmd[i] = split_cmd[i - 1];
+    split_cmd[1] = strdup("-l");
+    split_cmd[size + 1] = NULL;
+    return (split_cmd);
+}
+
 void list_action(server_utils_t *utils, char *param,
         client_sks_t *client)
 {
@@ -18,13 +35,12 @@ void list_action(server_utils_t *utils, char *param,
         return;
     }
     send_client_code(client->control, 150);
+    split_cmd = check_list_options(param);
     if ((pid = fork()) == 0) {
-        split_cmd = str_to_word_array(param, " \t");
-        free(split_cmd[0]);
-        split_cmd[0] = strdup("ls");
         dup2(client->data, 1);
         execvp(split_cmd[0], split_cmd);
     }
     close(client->data);
     send_client_code(client->control, 226);
+    free_2d_char_array(split_cmd);
 }
