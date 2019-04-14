e33d82f53dffc763be96c598f660e9b5fe447a18
Quentin Liardeaux <quentinliardeaux@localhost.localdomain>
2019-04-14 18:21:24 +0200

    Update pwd reply message.

--- FTPServer/sources/commands/pwd.c
+++ FTPServer/sources/commands/pwd.c
@@ -7,13 +7,19 @@
 
 #include "server.h"
 
+char *get_current_dir_name(void);
+
 void pwd_action(server_utils_t *utils,
         __attribute__((unused)) char *params,
         __attribute__((unused)) client_sks_t *client)
 {
+    char *pwd = NULL;
+
     if (client->is_connect != 2) {
         send_client_code(client->control, 530);
         return;
     }
-    send_client_message(client->control, utils->pwd, 257);
+    pwd = get_current_dir_name();
+    dprintf(client->control, "257 %s created.\n", pwd);
+    free(pwd);
 }
